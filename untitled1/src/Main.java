62367f305158ad9561e1666e60d51ecbee529eab
tripler4ze <124861523+tripler4ze@users.noreply.github.com>
2023-04-15 13:56:58 +0300

    Add files via upload

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -1,52 +1,53 @@
 
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
         public class Main {
 
     public static void main(String[] args) {
-        Scanner  scan = new Scanner(System.in);
+        Scanner  scanner = new Scanner(System.in);
         System.out.println("Выберите действие: 1) сумма\n" +
                 "2) разность\n" +
                 "3) произведение\n" +
                 "4) деление:");
 
-        int action =  scan.nextInt();
-
+        String action = scanner.nextLine();
 
         System.out.println(" Введите число:");
-        int num1 =  scan.nextInt();
+        int num1 =  scanner.nextInt();
+
         System.out.println(" Введите число:");
-        int num2 =  scan.nextInt();
+        int num2 =  scanner.nextInt();
+
         int res;
+
         switch (action) {
             case "1":
                 res = num1 + num2;
                 System.out.println("Результат:" + res);
                 break;
 
-            case  "2":
-                    res = num1 - num2;
-                    System.out.println("Результат:" + res);
-                    break;
+            case "2":
+                res = num1 - num2;
+                System.out.println("Результат:" + res);
+                break;
             case "3":
             res = num1 * num2;
             System.out.println("Результат:" + res);
             break;
+
             case "4":
             res = num1 / num2;
             System.out.println("Результат:" + res);
-            break;
+                break;
+            default:
+                System.out.println(" Ошибка:Ведите значение от 1 до 4");
         }
-        
+        System.out.println("Хотите выйти из проекта?  1)да" + "2)нет");
+        String choice= scanner.nextLine();
+        switch (choice) {
+            case "1":
 
-
-
-
-
-
-
-
-
+        }
 
 
 
